76c39b276e2a507f8089e95820c9b06494d230ff
GogglesTv <jalenmarquise.moore@gmail.com>
2024-12-20 22:17:24 -0600

    Created a function that creates divs and makes a grid out of them
    Applied grid and grid template columns styles to the container

--- script.js
+++ script.js
@@ -7,5 +7,18 @@ body.style.alignItems = "center";
 
 const grid = document.getElementById("container");
 grid.style.border = "3px solid black";
-grid.style.height = "256px";
-grid.style.width = "256px";
+grid.style.gridTemplateColumns = "repeat(16, auto)";
+grid.style.display = "grid";
+
+function createDiv() {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const div = document.createElement("div");
+      div.style.border = ".75px solid black";
+      div.style.padding = "10px";
+      grid.appendChild(div);
+    }
+  }
+}
+
+createDiv();
