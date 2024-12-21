69c99d1e79e71efb6cacc3a157fe5fa0e3ed6574
GogglesTv <jalenmarquise.moore@gmail.com>
2024-12-20 18:51:17 -0600

    Centered the title and the container for the drawing board

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const body = document.body;
+body.style.display = "flex";
+body.style.flexDirection = "column";
+body.style.alignItems = "center";
+
+const grid = document.getElementById("container");
+grid.style.border = "3px solid black";
+grid.style.height = "256px";
+grid.style.width = "256px";
