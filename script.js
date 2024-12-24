a26e0cb097e180ac18d4b03e433cabf56920bf8a
GogglesTv <jalenmarquise.moore@gmail.com>
2024-12-24 03:29:33 -0600

    Grid cells no longer have a border

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.style.border = ".25px solid lightgrey";
     cell.addEventListener("mouseover", () => {
       cell.style.backgroundColor = getRandomColor();
     });
