ed81e16e553172169351d94596512a45a7a8e16d
GogglesTv <jalenmarquise.moore@gmail.com>
2024-12-24 03:28:08 -0600

    Cell colors change randomly when hovered over
    Thickened the grid border

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 
 let gridColor = "black";
 
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function createGrid(size) {
   const grid = document.getElementById("container");
   grid.innerHTML = "";
@@ -11,9 +20,9 @@ function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.style.border = ".75px solid";
+    cell.style.border = ".25px solid lightgrey";
     cell.addEventListener("mouseover", () => {
-      cell.style.backgroundColor = gridColor;
+      cell.style.backgroundColor = getRandomColor();
     });
     grid.appendChild(cell);
   }
